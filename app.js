2cf4fcea5bf45f5b51e1ed14f456aa712569b0ed
yashVSawant <yashsawant23910@gmail.com>
2023-12-19 15:16:13 +0530

    new branch cicdDeployment

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
+
 const app = express();
 
 const sequelize = require('./util/database');
@@ -20,7 +22,7 @@ const order = require('./model/order');
 const resetPasswordRequest = require('./model/FPRequest');
 const reportUrl = require('./model/reportUrl');
 
-const accessLlogStrem = fs.createWriteStream(
+const accessLogStrem = fs.createWriteStream(
     path.join(__dirname,'access.log'),
     {flages:'a'}
 );
@@ -37,7 +39,8 @@ resetPasswordRequest.belongsTo(user);
 user.hasMany(reportUrl);
 reportUrl.belongsTo(user);
 
-app.use(morgan('combined',{stream: accessLlogStrem}))
+app.use(helmet());
+app.use(morgan('combined',{stream: accessLogStrem}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
@@ -48,6 +51,9 @@ app.use('/expence',expenceRoute);
 app.use('/purchase',authenticateRoute);
 app.use('/premium',premiumRoute);
 app.use('/password',passwordRoute);
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,`frontend/html/${req.url}`))
+})
 
 app.use((req,res,next)=>{
     res.status(404).send('Error: 404');
